853f295154a3dc3ff4a7e3bc571d7dc7ba5429fe
yazan hamamdi <120384088+yazan-hamamdi@users.noreply.github.com>
2024-03-30 02:34:13 +0300

    New addition to...and fixing some problems

---
+++ ISSHAR.DAL/Repositories/BookingRepository.cs
@@ -0,0 +1,42 @@
+﻿using ISSHAR.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISSHAR.DAL.Repositories
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly AppDbContext _context;
+
+        public async Task AddAsync(Booking booking)
+        {
+            await _context.Bookings.AddAsync(booking);
+            await SaveChangesAsync();
+        }
+        public async Task DeleteAsync(Booking booking)
+        {
+            _context.Bookings.Remove(booking);
+            await SaveChangesAsync();
+        }
+        public async Task<ICollection<Booking>> GetAllAsync()
+        {
+            return await _context.Bookings.AsNoTracking().ToListAsync();
+
+        }
+        public async Task<Booking> GetByIdAsync(int id)
+        {
+            return await _context.Bookings.AsNoTracking().FirstOrDefaultAsync(a => a.BookingId == id);
+
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Booking booking)
+        {
+            _context.Bookings.Update(booking);
+            await SaveChangesAsync();
+        }
+    }
+}
