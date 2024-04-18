6bfb7e4d2859ab8a1e347bdcf0e749462c36b794
IbraHeemALBilal <ibraheemataiwi123@gmail.com>
2024-04-18 22:11:01 +0300

    Edit User table

--- ISSHAR.Application/DTOs/UserDTOs/UserDTO.cs
+++ ISSHAR.Application/DTOs/UserDTOs/UserDTO.cs
@@ -2,10 +2,15 @@
 {
     public class UserDTO
     {
-        public string FullName { set; get; }
+        public string FirstName { get; set; }
+        public string FatherName { get; set; }
+        public string GrandFatherName { get; set; }
+        public string FamilyName { get; set; }
+        public string ImageUrl { get; set; }
         public string Password { set; get; }
         public string Email { set; get; }
         public DateTime DateOfBirth { get; set; }
+        public string City { set; get; }
         public string Gender { set; get; }
         public string Role { get; set; }
     }
