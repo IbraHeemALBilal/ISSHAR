e2ba6ede5ade9700be77a839597d27eefaf7264e
yazan hamamdi <120384088+yazan-hamamdi@users.noreply.github.com>
2024-04-28 13:30:56 +0300

    xxx

---
+++ ISSHAR.DAL/Repositories/CardTempleteRepository.cs
@@ -0,0 +1,42 @@
+﻿using ISSHAR.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISSHAR.DAL.Repositories
+{
+    public class CardTempleteRepository : ICardTempleteRepository
+    {
+        private readonly AppDbContext _context;
+        public CardTempleteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(CardTemplet cardTemplet)
+        {
+            await _context.CardTemplets.AddAsync(cardTemplet);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(CardTemplet cardTemplet)
+        {
+            _context.CardTemplets.Remove(cardTemplet);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<ICollection<CardTemplet>> GetAllAsync()
+        {
+            return await _context.CardTemplets
+                .OrderBy(c => c.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<CardTemplet> GetByIdAsync(int id)
+        {
+            return await _context.CardTemplets.AsNoTracking().FirstOrDefaultAsync(a => a.CardTempletId == id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ ISSHAR.DAL/Repositories/InviteRepository.cs
@@ -0,0 +1,47 @@
+﻿
+
+using ISSHAR.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISSHAR.DAL.Repositories
+{
+    public class InviteRepository : IInviteRepository
+    {
+        private readonly AppDbContext _context;
+        public InviteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Invite invite)
+        {
+            await _context.invites.AddAsync(invite);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> CheckIfInvitedBeforAsnyc(int senderId, int receiverId, int cardId)
+        {
+            return await _context.invites
+          .AnyAsync(i => i.SenderId == senderId && i.ReceiverId == receiverId && i.CardId == cardId);
+        }
+
+        public async Task<Invite> GetByIdAsync(int id)
+        {
+            return await _context.invites.AsNoTracking().Include(c => c.Sender).FirstOrDefaultAsync(a => a.InviteId == id);
+        }
+
+        public async Task<ICollection<Hall>> GetByReceiverIdAsync(int receiverId)// There may be problems
+        {
+            return (ICollection<Hall>)await _context.invites
+        .AsNoTracking()
+        .Where(i => i.ReceiverId == receiverId)
+        .Include(c => c.Sender)
+        .ToListAsync();
+
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
