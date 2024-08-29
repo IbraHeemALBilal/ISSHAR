496bd5012ccab560456056b49d0f7445dad79753
IbraHeemALBilal <ibraheemataiwi123@gmail.com>
2024-08-29 12:36:38 +0300

    Adding White List to check if token still valid or not

---
+++ ISSHAR.API/Middlewares/JwtWhitelistMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using ISSHAR.API.Interfaces;
+
+namespace ISSHAR.API.Middlewares
+{
+    public class JwtWhitelistMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IJwtWhitelistService _jwtWhitelistService;
+        public JwtWhitelistMiddleware(RequestDelegate next, IJwtWhitelistService jwtWhitelistService)
+        {
+            _next = next;
+            _jwtWhitelistService = jwtWhitelistService;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+
+            if (!string.IsNullOrEmpty(token) && !await _jwtWhitelistService.IsTokenWhitelistedAsync(token))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized: Token is not whitelisted.");
+                return;
+            }
+            await _next(context);
+        }
+    }
+}
